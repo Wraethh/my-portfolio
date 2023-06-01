6093c31db92bacc3d351f82ae6bba70a43b363f9
Guillaume Lamourec <parkerpool@live.fr>
2023-06-01 18:54:40 +0200

    some more work on projects section

---
+++ src/components/ProjetPics.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import styles from "./ProjectPics.module.css";
+
+export default function ProjetPics({ handleClick, ...project }) {
+  return (
+    <div className={styles.projectPics}>
+      <div>
+        <img
+          src={project.displayedImage}
+          alt={`displayed image of ${project.title} project`}
+        />
+      </div>
+      <div>
+        {project.images.map((image) => (
+          <img
+            key={image.id}
+            src={image.src}
+            onClick={handleClick}
+            alt={`preview picture ${image.id} of ${project.title} project`}
+            className={project.project}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+ProjetPics.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,87 @@
+import { useState } from "react";
+import Project from "../components/Project";
 import styles from "./Projects.module.css";
 
 export default function Projects() {
-  return <div className={styles.projects}>Projects</div>;
+  const [displayedImg, setDisplayedImg] = useState({
+    animalz: "",
+    tradin: "",
+    color: "",
+    gloomy: "",
+  });
+
+  const projectData = [
+    {
+      id: 1,
+      title: "Animalz",
+      project: "animalz",
+      duration: "36 hours",
+      description: "",
+      stack: [],
+      displayedImage: displayedImg.animalz,
+      images: [
+        { id: 1, src: "" },
+        { id: 2, src: "" },
+        { id: 3, src: "" },
+      ],
+    },
+    {
+      id: 2,
+      title: "Tradin' Impact",
+      project: "tradin",
+      duration: "4 weeks",
+      description: "",
+      stack: [],
+      displayedImage: displayedImg.tradin,
+      images: [
+        { id: 1, src: "" },
+        { id: 2, src: "" },
+        { id: 3, src: "" },
+      ],
+    },
+    {
+      id: 3,
+      title: "Color Flipper",
+      duration: "1 day",
+      description: "",
+      stack: [],
+      displayedImage: displayedImg.color,
+      images: [
+        { id: 1, src: "" },
+        { id: 2, src: "" },
+        { id: 3, src: "" },
+      ],
+    },
+    {
+      id: 4,
+      title: "Gloomy",
+      duration: "2 weeks",
+      description: "",
+      stack: [],
+      displayedImage: displayedImg.gloomy,
+      images: [
+        { id: 1, src: "" },
+        { id: 2, src: "" },
+        { id: 3, src: "" },
+      ],
+    },
+  ];
+
+  const handleClick = (e) => {
+    setDisplayedImg({ ...displayedImg, [e.target.className]: e.target.src });
+  };
+
+  // const carousel = document.getElementsByClassName("projects");
+
+  // const handleMouseMove = (e) => {
+  //   carousel.scrollLeft = e.pageX;
+  // };
+
+  return (
+    <div className={styles.projects}>
+      {projectData.map((project) => (
+        <Project key={project.id} {...project} handleClick={handleClick} />
+      ))}
+    </div>
+  );
 }
